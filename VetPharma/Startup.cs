a8e9b49e3f6ecf0c887478433f70d381c9ad8679
SuganthMani <suganth.mani@gmail.com>
2014-07-30 13:16:27 +0100

    Added solution

---
+++ VetPharma/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VetPharma.Startup))]
+namespace VetPharma
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
